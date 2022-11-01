c392a3546a6aa1dba6d47def33f42c9ab8913d4c
sinataghia <116915563+sinataghia@users.noreply.github.com>
2022-11-01 09:38:46 -1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showString("Hello!")
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Ghost)
+music.playMelody("G F E B B E F G ", 129)
+music.playMelody("B A G F G F G A ", 120)
+music.playMelody("- - - - - - - - ", 120)
+led.unplot(93, 22)
+basic.forever(function () {
+MesDpadButtonInfo
+    
+})
